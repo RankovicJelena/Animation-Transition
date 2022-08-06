62f12e3a304ea83f7725371786fb69ac965e8140
Jelena <jelenarankovic0@gmail.com>
2022-08-06 21:50:00 +0200

    animation with transition component

--- src/App.vue
+++ src/App.vue
@@ -91,7 +91,36 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
-@keyframes slide-fade {
+.v-enter-from {
+  /* opacity: 0;
+  transform: translateY(-30px); */
+}
+
+.v-enter-active {
+  animation: slide-scale 0.3s ease-out;
+}
+
+.v-enter-to {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+.v-leave-from {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+.v-leave-active {
+  /* transition: all 0.3s ease-in; */
+  animation: slide-scale 0.3s ease-out;
+}
+
+.v-leave-to {
+  /* opacity: 0;
+  transform: translateY(30px); */
+}
+
+@keyframes slide-scale {
   0% {
     transform: translateX(0) scale(1);
   }
